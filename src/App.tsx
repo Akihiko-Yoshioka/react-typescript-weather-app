34c971f4a0f0946045fe750376d7280269551abd
A.Yoshioka <akihiko.yoshioka@pro-spire.co.jp>
2024-06-22 23:24:34 +0900

    add Loading.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import Title from "./components/Title"
 import Form from "./components/Form"
 import Results from "./components/Results"
+import Loading from "./components/Loading"
 
 type ResultsState = {
   county: string
@@ -12,6 +13,7 @@ type ResultsState = {
 }
 
 const App = () => {
+  const [loading,setLoading]=useState<Boolean>(false)
   const [city,setCity]=useState<string>("")
   const [results, setResults] = useState<ResultsState>({
     county: "",
@@ -23,6 +25,7 @@ const App = () => {
 
   const getWeather = (e: React.FormEvent<HTMLElement>) => {
       e.preventDefault()
+      setLoading(true)
       fetch(`http://api.weatherapi.com/v1/current.json?key=4bdec65faabd40f2948150752241906&q=${city}&aqi=no`)
         .then(res => res.json())
         .then(data => {
@@ -33,15 +36,18 @@ const App = () => {
             conditionText:data.current.condition.text,
             icon:data.current.condition.icon
         })
+        setLoading(false)
+        setCity("")
     })
+    .catch(() => alert("エラーが発生しました.ページをリロードして、もう一度入力してください。"))
   }
 
   return (
     <div className="wrapper">
       <div className="container">
         <Title/>
-        <Form setCity={setCity} getWeather={getWeather}/>
-        <Results results={results}/>
+        <Form setCity={setCity} getWeather={getWeather} city={city}/>
+        {loading ? <Loading/> : <Results results={results}/>}
       </div>
     </div>
   )
---
+++ src/components/Loading.tsx
@@ -0,0 +1,4 @@
+
+const Loading=()=><h3>通信中</h3>
+
+export default Loading
